44a3fde8a573bf3a882bf6cea3dcd46fbb5fb645
rafaelalmeida9 <rafaeldealmeida@usp.br>
2024-05-26 11:00:05 -0300

    its good

--- ihavesufferedenough.c
+++ ihavesufferedenough.c
@@ -5,10 +5,10 @@ int main(){
     int n; scanf("%d", &n);
 
     for(int i=0;i<n;i++){
-        for(int j=0;j<i-1;j++){
+        for(int j=0;j<n-i-1;j++){
             printf(" ");
         }
-        for(int j=i-1;j<i;j++){
+        for(int j=n-i-1;j<n;j++){
             printf("*");
         }
         printf("\n");
